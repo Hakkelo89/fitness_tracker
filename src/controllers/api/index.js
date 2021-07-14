f5911b31aed2c299091ee7c1d1cb332c2555e16f
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-07-14 20:42:36 +0100

    controllers api and view js file coded with render stats page and render exerces page

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,1 +1,54 @@
 const { Workout } = require("../../models");
+
+// find all workouts in database
+const getAllWorkouts = async (req, res) => {
+  try {
+    const allWorkoutsData = await Workout.find();
+    return res.status(200).json(allWorkoutsData);
+  } catch (error) {
+    console.info(error.message);
+    return res.status(500).json({ error: "Failed to get workouts." });
+  }
+};
+
+// find current workout by id to update in database
+const changeWorkout = async (req, res) => {
+  try {
+    const workoutData = await Workout.findByIdAndUpdate(req.params.id, {
+      $push: { exercises: req.body },
+    });
+    return res.status(200).json(workoutData);
+  } catch (error) {
+    console.info(error.message);
+    return res.status(500).json({ error: "Failed to update workout." });
+  }
+};
+
+// add new workout to database
+const createWorkout = async (req, res) => {
+  try {
+    const workoutData = await Workout.create({});
+    return res.status(200).json(workoutData);
+  } catch (error) {
+    console.info(error.message);
+    return res.status(500).json({ error: "Failed to create workout." });
+  }
+};
+
+// get workouts for stats page
+const getWorkoutRange = async (req, res) => {
+  try {
+    const workoutData = await Workout.find();
+    return res.status(200).json(workoutData);
+  } catch (error) {
+    console.info(error.message);
+    return res.status(500).json({ error: "Failed to get workout." });
+  }
+};
+
+module.exports = {
+  getAllWorkouts,
+  changeWorkout,
+  createWorkout,
+  getWorkoutRange,
+};
