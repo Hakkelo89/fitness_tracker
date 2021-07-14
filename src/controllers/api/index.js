6c28a8cd17a4d29c31e971d78d6fe02e5b8f8483
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-07-14 23:01:04 +0100

    getAllWorkouts and AggregatedWorkout is updated

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -3,8 +3,10 @@ const { Workout } = require("../../models");
 // find all workouts in database
 const getAllWorkouts = async (req, res) => {
   try {
-    const allWorkoutsData = await Workout.find();
-    return res.status(200).json(allWorkoutsData);
+    const lastWorkout = await Workout.aggregate([
+      { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+    ]);
+    return res.status(200).json(lastWorkout);
   } catch (error) {
     console.info(error.message);
     return res.status(500).json({ error: "Failed to get workouts." });
@@ -38,7 +40,11 @@ const createWorkout = async (req, res) => {
 // get workouts for stats page
 const getAggregatedWorkout = async (req, res) => {
   try {
-    const workoutData = await Workout.find();
+    const workoutData = await Workout.aggregate([
+      { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+    ])
+      .sort({ _id: -1 })
+      .limit(7);
     return res.status(200).json(workoutData);
   } catch (error) {
     console.info(error.message);
